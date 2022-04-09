a42238a8ce42c6f242c8e185a0f4b40ca6188ae1
natasha1865 <natasha-tania1@hotmail.com>
2022-04-09 13:01:10 +0200

    move files to challenge folder

---
+++ InteractiveRainbow/static/jsdesigns.js
@@ -0,0 +1,31 @@
+
+
+function myFunc1() {
+  document.getElementById("Button1").style.backgroundColor = "red";
+  document.getElementById("Button1").innerHTML= "RED"
+}
+
+function myFunc2() {
+  document.getElementById("Button2").style.backgroundColor = "orange";
+  document.getElementById("Button2").innerHTML= "ORANGE"
+}
+function myFunc3() {
+  document.getElementById("Button3").style.backgroundColor = "yellow";
+  document.getElementById("Button3").innerHTML= "YELLOW"
+}
+function myFunc4() {
+  document.getElementById("Button4").style.backgroundColor = "green";
+  document.getElementById("Button4").innerHTML= "GREEN"
+}
+function myFunc5() {
+  document.getElementById("Button5").style.backgroundColor = "blue";
+  document.getElementById("Button5").innerHTML= "BLUE"
+}
+function myFunc6() {
+  document.getElementById("Button6").style.backgroundColor = "indigo";
+  document.getElementById("Button6").innerHTML= "INDIGO"
+}
+function myFunc7() {
+  document.getElementById("Button7").style.backgroundColor = "violet";
+  document.getElementById("Button7").innerHTML= "VIOLET"
+}
